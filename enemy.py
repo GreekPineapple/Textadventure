027f02ab2be8b4ebc0a23c7e67235833d16fd699
GreekPineapple <108550680+GreekPineapple@users.noreply.github.com>
2022-07-22 23:12:23 +0200

    fix(enemy.fight): check if item in inventory

--- enemy.py
+++ enemy.py
@@ -8,7 +8,7 @@ class Enemy:
         print("Stärke: " + str(self.strength) )
 
     def fight(self, player):
-        print("KAMPF!")
+        print("Oh nein, du wirst angegriffen! :(")
         while self.lives > 0:
             print("Du kannst\na) Angreifen\nb) Verteidigen\nc) Trank nehmen")
             option = input(">")
@@ -16,29 +16,31 @@ class Enemy:
                 self.lives -= player.strength
                 print("Du hast angegriffen, der gegner hat noch " + str(self.lives) + " Leben")
             elif option.lower() == "b":
-                pass
+                print("Du bist dem Gegner ausgewichen und verlierst keine Leben")
             elif option.lower() == "c":
                 print("Du hast folgende Tränke zur verfügung: " + str(player.inventory))
                 potion = input("Welchen Trank möchtest du nutzen?")
-                if potion == "eins" and "eins" in player.inventory:
-                    print("true")
+                if potion in player.inventory:
+                    print("jupii")
+                else: 
+                    print("not in you inventory")
             player.lives -= self.strength
             print("Der Gegner greift an, du hast noch " + str(player.lives) + " Leben")
 
 class Player(Enemy):
-    def __init__(self, lives, strength, inventory, startposition):
+    def __init__(self, lives, strength, inventory, positionNow):
         self.inventory = inventory
-        self.startposition = startposition
+        self.positionNow = positionNow
         super().__init__(lives, strength)
 
-    def move(self):
+    def move(self): #laufen möglich, geheimwege fehlen noch
         a = [[10, 11, 12, 13], [20, 21, 22, 23], [30, 31, 32, 33], [40, 41, 42, 43]]
         #for i in range(len(a)):
-        #    for j in range(len(a[i])):
-        #        print(a[i][j], end=' ')
-        #    print()
-        print(self.position)
-        position = self.startposition
+         #   for j in range(len(a[i])):
+          #      print(a[i][j], end=' ')
+           # print()
+        position = self.positionNow
+        
         print("In welche Richtung möchtest du gehen? (N/O/S/W)")
         direction = input(">")
         if direction.lower() == "n":
@@ -75,5 +77,5 @@ class Player(Enemy):
                 position -= 1
         else:
             print("Ungültige Eingabe")
-        self.startposition = position
-        print(position)
+        self.positionNow = position
+        
