1fa1f97703390ea2b7fb3ead7c8b8e05fa26fcf1
GreekPineapple <anna-moutidis@t-online.de>
2024-07-11 00:17:19 +0200

    fix(main): change some values

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import random, time
+import random
 from person import Player, Villain
 from map import *
 from notes import *
 map = Map(4,4)
-me = Player(200, 10, "myself", ["eins", "golem überreste", "heiltrank", "vogel überreste", "bombe", "Gutschein", "goblin überreste", "schussattacke", "bombe", "wizard überreste"], 42) #Start: Townhall
+me = Player(200, 40, "myself", [], 42) #Start: Townhall
 townhall = TownHall()
 woods = Woods("open")
 wf = Waterfall("open")
@@ -14,10 +14,10 @@ birdhouse = BirdHouse("open")
 notes = Notes()
 
 #the smaller the resistance, the smaller the damage (Muss vielleicht *-1 machen, damit sprachlich sinn ergibt xD)
-goblin = Villain("Goblin", 40, 100, 1, [5, 0, 10], "goblin überreste")
-golem = Villain("Erdgolem", 100, 40, 4, [20, 15, 10], "golem überreste")
-wizard = Villain("Magier", 80, 80, 6, [5, 10, 15], "wizard überreste")
-luftGegner = Villain("Luftgegner", 60, 50, 2, [5, 20, 0], "vogel überreste")
+goblin = Villain("Goblin", 100, 50, 1, [5, 0, 10], "goblin überreste")
+golem = Villain("Erdgolem", 150, 20, 4, [20, 15, 10], "golem überreste")
+wizard = Villain("Magier", 120, 40, 6, [5, 10, 15], "wizard überreste")
+luftGegner = Villain("Luftgegner", 110, 25, 2, [5, 20, 0], "vogel überreste")
 
 boss = Villain("Boss", 150, 150, 15, ["a","b","c"], "special glitzer boss attacke")
 
@@ -71,24 +71,15 @@ def printposition(position): #TODO mach match-case draus
     else:
         print("You're out of map lul")
 
-#townhall.explore(me)
-me.boss(villains, boss)
-#me.fight(golem)
+
 # quest can be: open; active; done; 
-#map.printMap()
-#notes.read()
+map.printMap()
+notes.read()
 
 #TODO Stelle sicher, dass es verschiedene Arten von Angriffen gibt, die der Spieler verwenden kann, z.B. physische -, oder magische Angriffe
 # oder Statusveränderungen. Jeder Angriffstyp sollte seine eigenen Vor- und Nachteile haben, so dass der Spieler strategisch vorgehen muss.
 #TODO Zeit abbrechen wenn alle fragen durch sind
 #TODO maybe bei nicht bestandenem quiz, hinweis in die notes schreiben?
-#TODO Shop mit json machen
-
-# Für den Bosskampf gibt es keinen shop, man verwendet dinge die von einem normalen gegner gedroppt werden, nachdem man diesen besiegt. Man braucht aber spezial angriffe von jedem gegner dens gibt um den boss zu besiegen
-# Wenn man einen kampf mit dem normalen gegner verliert, verliert man auch ein leben. mit diesen leben (schon implementiert) geht man dann in den bosskampf. (außer man hat heilung)
-
-# Gegner droppen gutscheine oder deren üverreste also z.b. magier droppt mageier überreste, goblin droppt goblin überreste
-# gutscheine um ausrüstung und heilung zu kaufen
 
 block = False # avoids two fights after another
 while me.lives > 0:
