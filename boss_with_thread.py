e3a2321fc43912c9731041de7360e94ed8c1fa91
GreekPineapple <anna-moutidis@t-online.de>
2026-08-14 19:19:05 +0200

    commit some uncommited changes from centuries ago

--- boss_with_thread.py
+++ boss_with_thread.py
@@ -1,22 +1,49 @@
-import json, random, time, threading
+import time
+import threading
+import queue
 
 class BossWithThread:
     def __init__(self, name):
         self.name = name
+        self.player_inputs = queue.Queue()
+        self.running = True
 
-    def bossAction():
-        while True:
-            print("Boss macht was er will!")
-            time.sleep(3)
+    def bossAction(self):
+        while self.running:
+            try:
+                user_input = self.player_inputs.get(timeout=3)
+            except queue.Empty:
+                print("Boss macht was er will!")
+                continue
+
+            if user_input == "1":
+                print("Boss-Reaktion auf Input 1: Angriff!")
+            elif user_input == "5":
+                print("Boss-Reaktion auf Input 5: Verteidigung!")
+            elif user_input == "stop":
+                self.running = False
+                print("Boss stoppt den Kampf.")
+            else:
+                print(f"Boss versteht '{user_input}' nicht.")
+
+            time.sleep(0.5)
     
-    def playerAction():
-        print("Mach was:") 
-        while input() != "stop":
+    def playerAction(self):
+        print("Mach was: (z.B. 1, 5, stop)")
+        while self.running:
+            user_input = input().strip()
+            self.player_inputs.put(user_input)
+            if user_input == "stop":
+                print("Spieler hat gestoppt!")
+                break
             print("Mach was:")
-        print("Spieler hat gestoppt!")
 
-    if __name__ == "__main__":
-        thread = threading.Thread(target=bossAction)
-        thread2 = threading.Thread(target=playerAction)
-        thread.start()
-        thread2.start()
+
+if __name__ == "__main__":
+    game = BossWithThread("Endboss")
+    thread = threading.Thread(target=game.bossAction)
+    thread2 = threading.Thread(target=game.playerAction)
+    thread.start()
+    thread2.start()
+    thread.join()
+    thread2.join()
