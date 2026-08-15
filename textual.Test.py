4a530dc394ca5f363db2ba035be113ec8f1d65c0
GreekPineapple <anna-moutidis@t-online.de>
2026-08-15 22:01:06 +0200

    feat(textual): add 2 containers to the terminal

---
+++ textual.Test.py
@@ -0,0 +1,33 @@
+from textual.app import App
+from textual.widgets import Input, Label, Button, Digits, Placeholder, Static
+from textual import on
+import countdown, threading
+
+class FormApp(App):
+    def compose(self):
+        self.widget1 = Static("Blah bli blub")
+        yield self.widget1
+        self.widget2 = Static("Dupdidupdidup")
+        yield self.widget2
+
+    def on_mount(self) -> None:
+        self.screen.styles.layout = "horizontal"
+        
+        self.widget1.styles.width = "50%"
+        self.widget1.styles.height = "100%"
+        self.widget1.border_title = "Villain"
+        self.widget1.styles.border = ("heavy", "yellow")
+        self.widget1.styles.padding = (1,3)
+        
+        self.widget2.styles.width = "50%"
+        self.widget2.styles.height = "100%"
+        self.widget2.border_title = "Player"
+        self.widget2.styles.border = ("heavy", "blue")
+        self.widget2.styles.padding = (1,3)
+        
+if __name__ == "__main__":
+    countdownThread = threading.Thread(target=countdown.countdown, args=(10,))
+    countdownThread.start()
+    FormApp().run()
+    countdownThread.join()
+    
