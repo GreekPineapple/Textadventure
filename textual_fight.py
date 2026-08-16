c5c92dced71e6dda6efc24b83f4ec850ab3ad08c
GreekPineapple <anna-moutidis@t-online.de>
2026-08-16 14:24:56 +0200

    refactor(textual): rename file + change player and villain

--- textual_fight.py
+++ textual_fight.py
@@ -1,8 +1,7 @@
 import time
 
 from textual.app import App
-from textual.widgets import Input, Label, Button, Digits, Placeholder, Static
-from textual import on
+from textual.widgets import Static
 import countdown, threading
 
 class FormApp(App):
@@ -17,14 +16,14 @@ class FormApp(App):
         
         self.widget1.styles.width = "50%"
         self.widget1.styles.height = "100%"
-        self.widget1.border_title = "Villain"
-        self.widget1.styles.border = ("heavy", "yellow")
+        self.widget1.border_title = "Player"
+        self.widget1.styles.border = ("heavy", "blue")
         self.widget1.styles.padding = (1,3)
         
         self.widget2.styles.width = "50%"
         self.widget2.styles.height = "100%"
-        self.widget2.border_title = "Player"
-        self.widget2.styles.border = ("heavy", "blue")
+        self.widget2.border_title = "Villain"
+        self.widget2.styles.border = ("heavy", "yellow")
         self.widget2.styles.padding = (1,3)
         
         self.countdownThread = threading.Thread(target=countdown.countdown, args=(5, self.update_countdown))
@@ -32,7 +31,6 @@ class FormApp(App):
         
         self.villainThread = threading.Thread(target=self.villain_action, args=())
         self.villainThread.start()
-        
         
     def update_countdown(self, timer):
         self.call_from_thread(self.widget2.update, timer)
